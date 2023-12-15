e3bd5172add1d0237862f631419e48b474aa3757
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2023-12-15 19:20:47 +0100

    Create variables for questions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+
+/*
+
+1-Dichiarare una variabile nome
+2-Chiedere il nome all'utente
+3-Salvarlo in una variabile
+4-Dichiarare una variabile cognome
+5-Chiedere il cognome all'utente 
+6-Salvarlo in una variabile
+7-Dichiarare una variabile colore
+8-Chiadere il colore preferito all'utente
+9-Salvarlo in una variabile
+10-Dichiaro una variabile number
+11-Creo una variabile number
+12-Recupero l'elemento nell'HTML
+13-Costrisco il contenuto da inserire nell l'elemento
+14-Inserisco il contenuto nell'elemento
+
+*/
+
+
+/*Prova console*/
 console.log('JS OK')
+/*Svolgimento punti 1,2,3*/
+const firstName = prompt('Qual è il tuo nome?', 'Riccardo')
+console.log(firstName)
+
+/*Svolgimento punti 4,5,6*/
+const surname = prompt('Qual è il tuo cognome?', 'Garbo')
+console.log(surname)
+
+/*Svolgimento punti 7,8,9*/
+const color = prompt('Qual è il tuo colore preferito?', 'Rosso')
+console.log(color)
+
+
+
